628aaa44ba1f2358df4105fc84253f2d531dc63f
Oliver <oliver.redeyoff@gmail.com>
2019-10-31 23:43:39 +0000

    thanks arthur

---
+++ frontend/signin.js
@@ -0,0 +1,8 @@
+function verifyAccount(){
+  var info = document.getElementsByClassName('signin');
+  var email = info[0].value;
+  var password = info[1].value;
+
+  location.replace("main.html");
+
+}

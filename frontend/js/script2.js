6f71ab337eb5279460860f0a011b86cb0e554fd9
Oliver <oliver.redeyoff@gmail.com>
2019-11-01 06:20:19 +0000

    Loading jobs from backend

--- frontend/js/script2.js
+++ frontend/js/script2.js
@@ -1,5 +1,3 @@
-var oldcontent = document.getElementsByClassName('content profile');
-
 function loadName(){
   document.getElementById("username").innerHTML = document.cookie.substr(9);
 }
@@ -26,33 +24,20 @@ function showCourses() {
 }
 
 function getJobs() {
-  toSend = {
-    "employer": "cisco",
-    "title": "engineer"
+  var request = new XMLHttpRequest()
+  request.open('GET', 'https://europe-west2-betacuck.cloudfunctions.net/getAllJobs')
+	var data = null;
+
+  console.log("test");
+  request.onload = function () {
+    console.log("test");
+    data = JSON.parse(this.response)
+    console.log(data);
+
+    for(i = 1; i<data.length ; i++){
+      document.getElementById("jobList").innerHTML += "<div id='joblisting'><h2>" + data[i].job + " at " + data[i].employer + "</h2></div>";
+    }
+
   }
-
-  fetch('https://europe-west2-betacuck.cloudfunctions.net/getJob', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(toSend),
-  }).then((resp) => {
-    return resp.json();
-  }).then((text) => {
-    console.log(text);
-  }).catch((error) => {
-    console.log("error");
-    console.log(error);
-  });
-
-
-  // var request = new XMLHttpRequest()
-  //
-  // request.open('POST', 'https://cors-anywhere.herokuapp.com/https://europe-west2-betacuck.cloudfunctions.net/getJob', true)
-  //
-  // request.onload = function() {
-  //   console.log(request);
-  // }
-  // request.send(JSON.stringify(toSend))
+  request.send()
 }

dac0b79aa8e7e2677141bc06cc9419d96702af21
Will Cruse <willcruse123@gmail.com>
2019-11-01 01:42:00 -0400

    I live

--- frontend/webExScript.js
+++ frontend/webExScript.js
@@ -1,32 +1,37 @@
-API_KEY = "ODAyYTE2MmYtYzYwMy00YTE5LWE3MjgtZmIxMzgwMDQxNTM4ZjVjODViNzktMmEx_PF84_consumer";
+API_KEY = "Bearer ODAyYTE2MmYtYzYwMy00YTE5LWE3MjgtZmIxMzgwMDQxNTM4ZjVjODViNzktMmEx_PF84_consumer";
 
 function sendMessage(to, message) {
+    data = {
+        "toPersonEmail": to,
+        "text": message
+      }
     fetch("https://api.ciscospark.com/v1/messages", {
         method: 'POST',
         headers: {
             'Authorization': API_KEY,
-            'Origin': '127.0.0.1',
+            'Content-Type': "application/json"
         },
-        body: {
-            "toPersonEmail": to,
-            "text": message
-        }
+        body: JSON.stringify(data)
     }).then((resp) => {
-        console.log(resp.json());
+        return resp.json();
+    }).then((json) => {
+        console.log(json);
     }).catch((error) => {
         console.log(error);
     });
 }
 
 function checkMessages(from) {
-    fetch(("https://api.ciscospark.com/v1/messages/direct?"+from), {
+    fetch(("https://api.ciscospark.com/v1/messages/direct?personEmail=" + from), {
         method: 'GET',
         headers: {
             'Authorization': API_KEY,
-            'Origin': '127.0.0.1',
+            'Content-type': 'application/json'
         },
     }).then((resp) => {
-        console.log(resp.json());
+        return resp.json();
+    }).then((json) => {
+        console.log(json);
     }).catch((error) => {
         console.log(error);
     });
